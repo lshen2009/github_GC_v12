ded56660b6be08e64316841dca53ec6a6e69adaf
Lu Shen <lshen729@gmail.com>
2018-11-07 16:34:39 -0500

    lshen

---
+++ README.sh
@@ -0,0 +1,4 @@
+Code.GC12: the currently working directory
+Code.GC12_Nov4: Delete the last species and see if I can make the code run. But it does not work.
+Code.GC12_Nov7: Add LSHENA in the Standard.eqn and then delete it using KPP. Now the code works,
+                but not in an elegant way.
