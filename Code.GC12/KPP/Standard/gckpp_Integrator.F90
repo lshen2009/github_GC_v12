73b57bba6bd31f4397a7c70efb1f987087791298
Lu Shen <lshen729@gmail.com>
2018-11-12 00:18:59 -0500

    lshen

--- Code.GC12/KPP/Standard/gckpp_Integrator.F90
+++ Code.GC12/KPP/Standard/gckpp_Integrator.F90
@@ -103,23 +103,23 @@ SUBROUTINE INTEGRATE( TIN, TOUT, LS_type, &
      WHERE(RCNTRL_U(:) > 0) RCNTRL(:) = RCNTRL_U(:)
    END IF
    
-   !IF (LS_type==1) THEN
-   !  VAR_selected_1=VAR(select_ind_1)
-   !  CALL Rosenbrock(NVAR_1,VAR_selected_1,TIN,TOUT,   &
-   !      ATOL,RTOL,                &
-   !      RCNTRL,ICNTRL,RSTATUS,ISTATUS,IERR, &
-   !	 LU_NONZERO_1,NVAR_1,LU_CROW_1,LU_DIAG_1,LU_IROW_1,LU_ICOL_1, LS_type)
-   !	 VAR(select_ind_1)=VAR_selected_1
-   ! END IF
+   IF (LS_type==1) THEN
+     VAR_selected_1=VAR(select_ind_1)
+     CALL Rosenbrock(NVAR_1,VAR_selected_1,TIN,TOUT,   &
+         ATOL,RTOL,                &
+         RCNTRL,ICNTRL,RSTATUS,ISTATUS,IERR, &
+   	 LU_NONZERO_1,NVAR_1,LU_CROW_1,LU_DIAG_1,LU_IROW_1,LU_ICOL_1, LS_type)
+   	 VAR(select_ind_1)=VAR_selected_1
+    END IF
    
-   !IF (LS_type==2) THEN
+   IF (LS_type==2) THEN
      VAR_selected_2=VAR(select_ind_2)
      CALL Rosenbrock(NVAR_2,VAR_selected_2,TIN,TOUT,   &
          ATOL,RTOL,                &
          RCNTRL,ICNTRL,RSTATUS,ISTATUS,IERR, &
 		 LU_NONZERO_2,NVAR_2,LU_CROW_2,LU_DIAG_2,LU_IROW_2,LU_ICOL_2, LS_type)
 	  VAR(select_ind_2)=VAR_selected_2
-   !END IF
+   END IF
    
    !~~~> Debug option: show no of steps
    ! Ntotal = Ntotal + ISTATUS(Nstp)
