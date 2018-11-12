6c558e17f669db5334cd41790d78ff0bb2d302ad
Lulu Shen <lshen@rclogin07.rc.fas.harvard.edu>
2018-11-12 10:26:26 -0500

    lshen

--- README.sh
+++ README.sh
@@ -2,5 +2,6 @@ Code.GC12: the currently working directory
 Code.GC12_Nov4: Delete the last species and see if I can make the code run. But it does not work.
 Code.GC12_Nov7: Add LSHENA in the Standard.eqn and then delete it using KPP. Now the code works,
                 but not in an elegant way.
-Code.GC12_Nov8: Now the code works well based on Code.GC12_Nov7.
+Code.GC12_Nov8: Now the code works well based on Code.GC12_Nov7, but in an elegant way.
 Code.GC12_Nov9: The number of Functions that needs editting is reduced to 5. I test after deleting 6 species.
+Code.GC12_Nov10: I test two mechanisms, one for whole chem in the troposphere and the other for nonVoCs in the Statosphere. Now the simulation time can reduce 20%. . 
