2c95ad8600a3569ebac9cf61f6e93737c9de4a95
Lu Shen <lshen729@gmail.com>
2018-11-13 08:06:12 -0500

    lshen

--- Code.GC12/KPP/Standard/gckpp_Global.F90
+++ Code.GC12/KPP/Standard/gckpp_Global.F90
@@ -38,7 +38,7 @@ MODULE gckpp_Global
   REAL(kind=dp) :: VAR(NVAR)
   REAL(kind=dp) :: VAR_selected_1(NVAR_1)
   REAL(kind=dp) :: VAR_selected_2(NVAR_2)
-  REAL(kind=dp) :: VAR_deleted_2(LU_DEL_2)
+  REAL(kind=dp) :: VAR_deleted_2(NVAR_2-NVAR_1)
 ! FIX - Concentrations of fixed species (global)
   REAL(kind=dp) :: FIX(NFIX)
 ! VAR, FIX are chunks of array C
