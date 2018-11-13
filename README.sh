179572c1c861a0ff489f31121c3e6fb5a2c87813
Lulu Shen <lshen@rclogin08.rc.fas.harvard.edu>
2018-11-13 15:22:42 -0500

    lshen

--- README.sh
+++ README.sh
@@ -4,4 +4,6 @@ Code.GC12_Nov7: Add LSHENA in the Standard.eqn and then delete it using KPP. Now
                 but not in an elegant way.
 Code.GC12_Nov8: Now the code works well based on Code.GC12_Nov7, but in an elegant way.
 Code.GC12_Nov9: The number of Functions that needs editting is reduced to 5. I test after deleting 6 species.
-Code.GC12_Nov10: I test two mechanisms, one for whole chem in the troposphere and the other for nonVoCs in the Statosphere. Now the simulation time can reduce 20%. . 
+Code.GC12_Nov10: I test two mechanisms, one for whole chem in the troposphere and the other for nonVoCs in the Statosphere. Now the simulation time can reduce 20%.
+Code.GC12_Nov12: I defined var_selected and var_deleted in the Global variable. Now the simulation time can reduce 20%.
+Code.GC12_Nov13: Now the gckpp_Integrator can adapt to varying dimensions of fast and slow species.
