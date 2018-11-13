bc349287123a2d02ac53d745d3b68bea12e360b0
Lu Shen <lshen729@gmail.com>
2018-11-13 16:23:31 -0500

    lshen

--- Code.GC12/GeosCore/flexchem_mod.F90
+++ Code.GC12/GeosCore/flexchem_mod.F90
@@ -237,6 +237,9 @@ CONTAINS
     REAL(dp)               :: RSTATE     (                  20               )
     REAL(dp)               :: GLOB_RCONST(IIPAR,JJPAR,LLPAR,NREACT           )
     REAL(fp)               :: Before     (IIPAR,JJPAR,LLPAR,State_Chm%nAdvect)
+	REAL(fp)               :: LS_Prate     (IIPAR,JJPAR,LLPAR,NVAR) !lshen
+	REAL(fp)               :: LS_Lrate     (IIPAR,JJPAR,LLPAR,NVAR) !lshen
+	INTEGER                :: LS_Alltype   (IIPAR,JJPAR,LLPAR) !lshen
 
     ! For tagged CO saving
     REAL(fp)               :: LCH4, PCO_TOT, PCO_CH4, PCO_NMVOC
