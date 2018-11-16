80e811df5f64b9bb9e92587fc8037a2f3e8cbe18
Lu Shen <lshen729@gmail.com>
2018-11-16 12:51:36 -0500

    lshen

--- Code.GC12/KPP/Standard/gckpp_Integrator.F90
+++ Code.GC12/KPP/Standard/gckpp_Integrator.F90
@@ -71,7 +71,7 @@ SUBROUTINE INTEGRATE( TIN, TOUT, LS_type,LS_NSEL, LS_NDEL,Prate, Lrate, &
 
    REAL(kind=dp), INTENT(IN) :: TIN  ! Start Time
    REAL(kind=dp), INTENT(IN) :: TOUT ! End Time
-   LOGICAL, INTENT(IN)::flag
+   
    ! Optional input parameters and statistics
    INTEGER,       INTENT(IN),  OPTIONAL :: ICNTRL_U(20)
    REAL(kind=dp), INTENT(IN),  OPTIONAL :: RCNTRL_U(20)
@@ -123,20 +123,12 @@ SUBROUTINE INTEGRATE( TIN, TOUT, LS_type,LS_NSEL, LS_NDEL,Prate, Lrate, &
           RCNTRL,ICNTRL,RSTATUS,ISTATUS,IERR, &
 		  LU_NONZERO_2,NVAR_2,LU_CROW_2,LU_DIAG_2,LU_IROW_2,LU_ICOL_2, LS_type)
 		
-		!IF (flag) THEN
-		!	print *,"lshen_flag", 0.01/deltaT
-		!	print *,"VAR_deleted",VAR_deleted(1:10)
-		!	print *,"LS_P",LS_P(1:10)
-		!	print *,"LS_L",LS_L(1:10)
-		!END IF
 		WHERE(LS_L<=(0.01/deltaT))			
 			VAR_deleted=VAR_deleted+deltaT*(LS_P-LS_L*VAR_deleted)
 		ELSEWHERE
 			VAR_deleted=LS_P/LS_L+(VAR_deleted-LS_P/LS_L)*EXP(-LS_L*deltaT)
 		END WHERE
-		!IF (flag) THEN
-		!    print *,"VAR_deleted",VAR_deleted(1:10)
-		!END IF
+		
 	    VAR(select_ind_2)=VAR_selected
 	    VAR(delete_ind_2)=VAR_deleted
       CASE DEFAULT
