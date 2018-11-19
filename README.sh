9bae661d61f4a6fff8d81bb4b5534c6fedae8921
Lulu Shen <lshen@rclogin08.rc.fas.harvard.edu>
2018-11-19 00:13:20 -0500

    lshen

--- README.sh
+++ README.sh
@@ -9,3 +9,4 @@ Code.GC12_Nov12: I defined var_selected and var_deleted in the Global variable. 
 Code.GC12_Nov13: Now the gckpp_Integrator can adapt to varying dimensions of fast and slow species.
 Code.GC12_Nov15: Now the code can deal with both fast and slow species. But the simulation speed doesnot improve.
 Code.GC12_Nov17: Based on Code.GC12_Nov15, but the code becomes more elegant.
+Code.GC12_Nov18: Based on Code.GC12_Nov17, I have added all 10 types of chemistry regimes. But the speed does not improve.
