c746bcc39c7263a9224e3a9c1490e7ec750512b3
Lu Shen <lshen729@gmail.com>
2018-11-20 22:41:36 -0500

    lshen

--- Code.GC12/KPP/Standard/gckpp_Function.F90
+++ Code.GC12/KPP/Standard/gckpp_Function.F90
@@ -9133,7 +9133,7 @@ FUNCTION Determine_type (Prate,Lrate) RESULT( flag )
   PP(10)=SUM(Vdot(LS_ind_10))  
   WHERE (PP>=1) PP=1
   ap=sum(LS_bin_base*PP)
-  DO I=1,10
+  DO I=1,15
      IF (LS_group(I)==ap) THEN
 	    flag=I
 		CONTINUE
