4edd09e7d2a60bacb3a4b2e6e29a1a368bcd7882
Lulu Shen <lshen@rclogin03.rc.fas.harvard.edu>
2018-12-18 15:45:52 -0500

    lshen

--- README.sh
+++ README.sh
@@ -15,4 +15,5 @@ Code.GC12_Nov21: The code works for 15 tyeps of chemistry regimes.
 Code.GC12_Nov21_afternoon: The code works for 15 tyeps of chemistry regimes. I have reduced the number of input parameters.
 Code.GC12_Nov25: The code works for 15 tyeps of chemistry regimes. I consider both fast and slow species in FUN and Jac_SP functions.
 Code.GC12_Dec04: The code works for 15 tyeps of chemistry regimes. I consider both fast and slow species in FUN and Jac_SP functions. But the model may be wrong in clean regions and in the stratosphere.
-Code.GC12_Dec16: The code works for 15 tyeps of chemistry regimes. I consider both fast and slow species in FUN and Jac_SP functions. But the model may be wrong in clean regions and in the stratosphere. This is the version after my Dec05 group meeting.
+Code.GC12_Dec16: The code works for 15 tyeps of chemistry regimes. I consider both fast and slow species in FUN and Jac_SP functions. But the model may be wrong in clean regions and in the stratosphere. This is the version after my Dec05 group meeting.I find the Het Function may cause discrepancies. 
+Code.GC12_Dec18: Now I am trying to find the day-night line and assign this as the full-chem regime.
